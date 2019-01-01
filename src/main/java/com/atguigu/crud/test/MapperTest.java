49b6d46ea7d4ae3fb5d4ec84801debb9371c86ee
Sww@ <author.com>
2019-01-01 16:17:30 +0800

    ssm-curd

---
+++ src/main/java/com/atguigu/crud/test/MapperTest.java
@@ -0,0 +1,44 @@
+package com.atguigu.crud.test;
+
+import java.util.UUID;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.atguigu.crud.bean.Department;
+import com.atguigu.crud.bean.Employee;
+import com.atguigu.crud.dao.DepartmentMapper;
+import com.atguigu.crud.dao.EmployeeMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"classpath:applicationContext.xml"})
+public class MapperTest {
+
+	@Autowired 
+	DepartmentMapper departmentMapper;
+	@Autowired
+	EmployeeMapper employeeMapper;
+	
+	@Autowired
+	SqlSession sqlSession;
+	/**
+	 *   测试DepartmentMapper
+	 */
+	@Test
+	public void testCRUD() {
+		/*System.out.println(departmentMapper);
+		departmentMapper.insertSelective(new Department(null,"开发部"));
+		departmentMapper.insertSelective(new Department(null,"测试部"));*/
+		//employeeMapper.insertSelective(new Employee(null,"jerry","1","jerry@123.com",1));
+		EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+		for(int i = 0;i<1000;i++) {
+			String uid = UUID.randomUUID().toString().substring(0, 5)+i;
+			mapper.insertSelective(new Employee(null,uid,"1",uid+"@qq.com",1));
+		}
+		System.out.println("11");
+	}
+}
---
+++ target/ssm-crud-0.0.1-SNAPSHOT/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+ <jsp:forward page="/emps"></jsp:forward>
+<!-- <!DOCTYPE html>
+<html>
+<head>
+引入样式
+<script type="text/javascript" src="static/js/jquery-1.12.4.min.js"></script>
+<link href="static/bootstrap-3.3.7-dist/css/bootstrap.min.css" refl="stylesheet">
+<script src="static/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<title>Insert title here</title>
+</head>
+<body>
+	<button>按钮</button>
+</body>
+</html> -->
